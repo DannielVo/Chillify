4936d4c8dca5fa11cd81b365c1b2ef02a26c01c2
DannielVo <phattinhoc2017@gmail.com>
2026-01-28 10:43:18 +0700

    Feat: Set up routes and project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,43 @@
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./layouts/AuthLayout";
+import Login from "./pages/auth/Login";
+import SignUp from "./pages/auth/SignUp";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/home/Home";
+import SectionDetail from "./pages/home/SectionDetail";
+import Profile from "./pages/account/Profile";
+import ChangePassword from "./pages/account/ChangePassword";
+import MyPlaylist from "./pages/playlist/MyPlaylist";
+import PlaylistDetail from "./pages/playlist/PlaylistDetail";
+import SongDetail from "./pages/song/SongDetail";
+
 function App() {
-  return <>App</>;
+  return (
+    <Routes>
+      {/* Auth routes */}
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+      </Route>
+
+      {/* Main routes */}
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/section/:id" element={<SectionDetail />}></Route>
+
+        <Route path="/account/profile" element={<Profile />}></Route>
+        <Route
+          path="/account/change-password"
+          element={<ChangePassword />}
+        ></Route>
+
+        <Route path="/playlists" element={<MyPlaylist />}></Route>
+        <Route path="/playlists/:id" element={<PlaylistDetail />}></Route>
+
+        <Route path="/song/:id" element={<SongDetail />}></Route>
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
