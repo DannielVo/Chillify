32c841eb32b222ff80351a9675ea0494b0920fcb
DannielVo <phattinhoc2017@gmail.com>
2026-02-10 19:54:26 +0700

    Fix: Update padding for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full mt-auto bg-[#19282E] py-8 flex justify-center border-t border-white/5">
+    <footer className="w-full mt-auto bg-[#19282E] py-6 flex justify-center border-t border-white/5">
       <div className="text-slate-400 text-xs">
         <span>© 2026 Chillify. All rights reserved.</span>
       </div>
